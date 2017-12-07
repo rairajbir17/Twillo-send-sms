b0ae7ed3340a94e117bcfd44423f8af9aa34b10e
Rajbir Kaur <rajbir21001003@gmail.com>
2017-12-07 13:52:05 -0800

    Add files via upload

---
+++ handler.js
@@ -0,0 +1,59 @@
+'use strict';
+// Twilio Credentials 
+var accountSid = 'AC1b7838a81f684b386d12199e5fcd5be6';
+var authToken = '281035db8be9f6683037c52f154a95e1';
+var fromNumber = '+17787641536';
+const twilioA = require('twilio')(accountSid, authToken);
+
+module.exports.postprocess = (event,context,callback) => {
+ console.log('Running event');
+     // Send an SMS message to the number provided in the event data.
+    // End the lambda function when the send function completes.
+    SendSMS('+17789534545', 'Photo Successfully Uploaded', 
+                function (status) { context.done(null, status); });  
+
+  // event.Records.forEach((record) => {
+  //   const filename = record.s3.object.key;
+  //   const filesize = record.s3.object.size;
+  //   console.log(`New .png object has been created: ${filename} (${filesize} bytes)`);
+  // });
+};
+
+function SendSMS(to, body, completedCallback) {
+    
+    // The SMS message to send
+    var message = {
+        To: to, 
+        From: fromNumber,
+        Body: body,
+    };
+    var messageString = "Message Sent"
+    
+    twilioA.messages.create(message, (err, data) => {
+    if (err) {
+      const errRes = {
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+        },
+        statusCode: err.status,
+        body: JSON.stringify({
+          message: err.message,
+          error: err
+        }),
+      };
+      return completedCallback(null, errRes);
+    }
+  });
+    const response = {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+      body: JSON.stringify({
+        message: 'Sent message!',
+        data: data
+      }),
+    };
+
+    callback(null, response);
+};
